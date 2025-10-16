2e657161d1a30de203ade6873ec5b8590ade9f4c
Storterald <storterald@gmail.com>
2025-10-16 20:11:32 +0200

    improved awful 1 year old build system

--- build.py
+++ build.py
@@ -1,61 +1,46 @@
 import os
 import sys
+import json
 import subprocess
-from infos import *
+from argparse import ArgumentParser
 
-def getVersion() -> str:
-        if not os.path.exists(VERSION_DIR):
-                os.mkdir(VERSION_DIR)
-                subprocess.run(["git", "clone", "--depth", "1", "--no-checkout", GITHUB_REPO, "."], cwd=VERSION_DIR, shell=True)
-                subprocess.run(["git", "fetch", "--tags", "--depth", "1"], cwd=VERSION_DIR, shell=True)
-        else:
-                subprocess.run(["git", "pull", "--depth", "1"], cwd=VERSION_DIR, shell=True)
 
-        version: str = subprocess.check_output(["git", "for-each-ref", "--sort=-creatordate", "--format", "%(refname:short)", "refs/tags"], cwd=VERSION_DIR, shell=True)
-        version = version.decode("utf-8")
-        version = version[:version.find('\n')]
-        version = "1.0.0" if version == '' else version
-        print(f"Current extension version is: {version}")
+def fix_version() -> tuple[str, str]:
+        with open("package.json", 'r+', encoding="utf-8") as f:
+                data = json.load(f)
+                f.seek(0)
 
-        return version
+                if "version" not in data:
+                        data["version"] = "1.0.0"
+                        json.dump(data, f, indent=2)
+                        f.truncate()
+        
+        with open("package-lock.json", 'r+', encoding="utf-8") as f:
+                lock = json.load(f)
+                f.seek(0)
+                lock["version"]                 = data["version"]
+                lock["packages"][""]["version"] = data["version"]
+                json.dump(lock, f, indent=2)
+                f.truncate()
 
-def fixPackages() -> None:
-        # Update global variables for restorePackages()
-        global PACKAGE, PACKAGE_LOCK
+        return data["version"], data["name"]
 
-        # Replace --version in package.json
-        with open("package.json", 'r', encoding="utf-8") as f:
-                PACKAGE = f.read()
-        with open("package.json", 'w', encoding="utf-8") as f:
-                fixedPackage: str = PACKAGE.replace("--version", VERSION)
-                f.write(fixedPackage)
-
-        # Replace --version in package-lock.json
-        if (os.path.exists("package-lock.json")):
-                with open("package-lock.json", 'r', encoding="utf-8") as f:
-                        PACKAGE_LOCK = f.read()
-                with open("package-lock.json", 'w', encoding="utf-8") as f:
-                        fixedPackageLock: str = PACKAGE_LOCK.replace("--version", VERSION)
-                        f.write(fixedPackageLock)
-
-def restorePackages() -> None:
-        with open("package.json", 'w', encoding="utf-8") as f:
-                f.write(PACKAGE)
-        if (os.path.exists("package-lock.json")):
-                with open("package-lock.json", 'w', encoding="utf-8") as f:
-                        f.write(PACKAGE_LOCK)
 
 if __name__ == "__main__":
-        global VERSION;
-        VERSION: str = getVersion()
-
-        fixPackages()
+        if not os.path.exists("package.json"):
+                raise Exception("package.json is missing")
         
-        subprocess.run(["vsce", "package"], shell=True)
+        if not os.path.exists("package-lock.json"):
+                subprocess.run("npm i", shell=True)
 
-        restorePackages()
+        version, name = fix_version()
+        subprocess.run("vsce package", shell=True)
 
-        for flag in sys.argv[1:]:
-                match flag:
-                        case "--install":
-                                subprocess.run(["code", "--install-extension", f"{NAME}-{VERSION}.vsix"], shell=True)
+        parser = ArgumentParser(
+                prog="Extension Builder",
+                description="Builds the .vsix extension")
+        parser.add_argument("-I", "--install", action="store_true")
+        args = parser.parse_args()
+        
+        if args.install:
+                subprocess.run(f"code --install-extension {name}-{version}.vsix", shell=True)
